93bab49ef70d8595456ac4ade9dc40df090508ab
amlaksil <silamlakdesye@gmail.com>
2023-10-06 02:20:29 +0300

    Calculate the floor of a float number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module: 2-floor
+
+This moudle provides a function to calculate the floor
+of float number.
+"""
+
+
+def floor(n: float) -> int:
+    """Calculate the floor of a float number.
+    Args:
+        n (float): The input float number.
+    """
+    return int(n)
