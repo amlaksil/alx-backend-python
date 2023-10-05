b0b6a77de01f60fcb66669dc8c0ee6fd8ba10704
amlaksil <silamlakdesye@gmail.com>
2023-10-06 02:32:53 +0300

    Return string representation of the float number

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module: 3-to_str
+
+This module provides a function named to_str.
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of float number
+    Args:
+        n (float): Input number.
+    """
+    return str(n)
