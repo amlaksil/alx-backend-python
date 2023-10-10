705ae18d80633b307f3d5a34b488200acd736e58
amlaksil <silamlakdesye@gmail.com>
2023-10-10 18:11:41 +0300

    Implement task_wait_random async routine

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module: 1-concurrent_coroutines
+
+This module provides utility functions for asynchronous operations.
+"""
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Asynchronous routine that spawns wait_random n times with the specified
+    max_delay. It returns the list of delays in ascending order.
+
+    Args:
+        n (int): The number of times to spawn wait_random coroutine.
+        max_delay (int): The maximum delay in seconds for each wait_random
+        coroutine.
+
+    Returns:
+        List[float]: The list of delays (float values) in ascending order.
+    """
+    coroutines = [task_wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*coroutines)
+    return sorted(results)
