5efa81a417ff95118b64114e93cc9b01f44d5060
amlaksil <silamlakdesye@gmail.com>
2023-10-10 05:14:03 +0300

    Add async routine called wait_n

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Module: 1-concurrent_coroutines
+
+This module provides utility functions for asynchronous operations.
+"""
+
+import asyncio
+import heapq
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Asynchronous routine that spawns wait_random n times with the specified
+    max_delay. It returns the list of delays in ascending order.
+
+    Args:
+        n (int): The number of times to spawn wait_random coroutine.
+        max_delay (int): The maximum delay in seconds for each wait_random
+        coroutine.
+
+    Returns:
+        List[float]: The list of delays (float values) in ascending order.
+    """
+    delays = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        heapq.heappush(delays, delay)
+    return delays
