de37340d5f7926bcaf7775f9e3f921857ab5d530
amlaksil <silamlakdesye@gmail.com>
2023-10-31 03:43:52 +0300

    Add test_public_repos_url method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 This module contains a test class for the GithubOrgClient class.
 """
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 from typing import Any, Dict, Tuple
@@ -63,6 +63,34 @@ class TestGithubOrgClient(unittest.TestCase):
         # Assert that the result is the expected value
         expected_result: Dict[str, Any] = {"org_key": "org_value"}
         self.assertEqual(result, expected_result)
+
+    def test_public_repos_url(self):
+        """
+        Test the _public_repos_url property of GithubOrgClient.
+
+        This method verifies that the _public_repos_url property
+        returns the expected URL based on the mocked payload.
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError: If the test fails.
+        """
+        # Mock the org method to return a known payload
+        mock_payload = {"repos_url":
+                        "https://api.github.com/orgs/testorg/repos"}
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock(
+                return_value=mock_payload)):
+            # Create an instance of GithubOrgClient
+            client = GithubOrgClient("testorg")
+
+            # Access the _public_repos_url property
+            result = client._public_repos_url
+
+            # Assert that the result is the expected URL
+            expected_url = "https://api.github.com/orgs/testorg/repos"
+            self.assertEqual(result, expected_url)
 
 
 if __name__ == '__main__':
