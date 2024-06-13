c63481400450e3e9a8f9c300bb58b11f12eef2e5
amlaksil <silamlakdesye@gmail.com>
2024-06-13 10:05:43 +0300

    Add type annotations for safely_get_value function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
 """
-Module:
+This module provides a function for safely retrieving values from a dictionary.
 """
-from typing import TypeVar, Mapping, Any, Optional, Union
 
-K = TypeVar('K')
-V = TypeVar('V')
+from typing import TypeVar, Mapping, Any, Union
+
+T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[K, V], key: K, default: Optional[V] = None)-> Union[V, Optional[V]]:
-    """
-    Safely retrieves a value from a dictionary based on the given key.
-
-    If the key exists in the dictionary, the corresponding value is returned.
-    If the key does not exist, the default value is returned.
+def safely_get_value(
+        dct: Mapping, key: Any, default: Union[T, None] = None
+        ) -> Union[Any, T]:
+    """Safely retrieves a value from a dictionary.
 
     Args:
-        dct (Mapping[K, V]): The dictionary-like object to retrieve the
-        value from.
-        key (K): The key to look up in the dictionary.
-        default (Optional[V], optional): The default value to return
-        if the key does not exist. Defaults to None.
+        dct (Mapping): The dictionary to retrieve the value from.
+        key (Any): The key whose value needs to be retrieved.
+        default (Union[T, None], optional): The default value to return
+        if the key is not found. Defaults to None.
 
     Returns:
-        Union[V, Optional[V]]: The value corresponding to the key
-        if it exists in the dictionary, or the default value if provided.
+        Union[Any, T]: The value corresponding to the key if it exists in
+        the dictionary, otherwise the default value.
+
+    Example:
+    >>> dct = {'a': 1, 'b': 2, 'c': 3}
+    >>> safely_get_value(dct, 'a')
+    1
+    >>> safely_get_value(dct, 'd', default='Not found')
+    'Not found'
     """
     if key in dct:
         return dct[key]
